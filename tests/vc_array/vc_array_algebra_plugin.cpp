df7d8996b3a4c9d070e44e66c8fca64f5886b2ba
beomki-yeo <beomki.yeo@gmail.com>
2021-10-19 09:07:47 -0700

    rebase

---
+++ tests/vc_array/vc_array_algebra_plugin.cpp
@@ -0,0 +1,9 @@
+/** Algebra plugins library, part of the ACTS project (R&D line)
+ * 
+ * (c) 2020 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
+#include "algebra/definitions/vc_array.hpp"
+#include "test_plugin.inl"

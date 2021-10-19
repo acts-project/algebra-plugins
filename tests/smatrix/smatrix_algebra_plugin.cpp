cac9ef4c0ae5093505a48864ce5c1baed5e7c134
Attila Krasznahorkay <30694331+krasznaa@users.noreply.github.com>
2021-10-19 15:42:39 +0200

    Repository Re-organisation, main branch (2021.10.15.) (#27)
    * Drastically re-organised the code in the repository.
    
    Moved all source files into locations that I felt would be more
    reasonable.
    
    Re-wrote the CMake configuration to be more friendly towards being
    included by other projects, and to make it possible to fine-tune
    the build configuration of the project a bit better.
    
    Re-wrote how "externals" would be set up by the project.
    
    Updated the CI configuration to test the code in more configurations,
    and fixed the name of the "main branch".
    
    * Updated the README to describe the new set of CACHE variables.
    
    * Suppressing developer warnings for all externals by default.
    
    Done in a way that would still respect explicit -Wdev / -Wno-dev
    arguments to CMake from the command line.

---
+++ tests/smatrix/smatrix_algebra_plugin.cpp
@@ -0,0 +1,9 @@
+/** Algebra plugins library, part of the ACTS project
+ * 
+ * (c) 2020 CERN for the benefit of the ACTS project
+ * 
+ * Mozilla Public License Version 2.0
+ */
+
+#include "algebra/definitions/smatrix.hpp"
+#include "test_plugin.inl"
--- tests/unit_tests/array/array_algebra_plugin.cpp
+++
@@ -1,9 +0,0 @@
-/** Algebra plugin library, part of the ACTS project
- * 
- * (c) 2020 CERN for the benefit of the ACTS project
- * 
- * Mozilla Public License Version 2.0
- */
-
-#include "algebra/definitions/std_array.hpp"
-#include "tests/common/test_plugin.inl"

8009ddb9e81215d7be243aa409535d46714323ec
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2021-10-20 09:22:17 +0200

    Introduced code format checking for the repository
    Updating all source files to follow these new formatting rules
    along the way.

--- tests/eigen/eigen_algebra_plugin.cpp
+++ tests/eigen/eigen_algebra_plugin.cpp
@@ -1,9 +1,12 @@
 /** Algebra plugins library, part of the ACTS project
- * 
+ *
  * (c) 2020 CERN for the benefit of the ACTS project
- * 
+ *
  * Mozilla Public License Version 2.0
  */
 
+// Project include(s).
 #include "algebra/definitions/eigen.hpp"
+
+// Test include(s).
 #include "test_plugin.inl"

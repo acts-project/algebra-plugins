6a6a9b0b81665c6c7685fa5eed1f1d6166eedd68
wermos <63574588+wermos@users.noreply.github.com>
2022-10-31 11:54:30 +0530

    Wrote the vector functions in terms of Fastor functions.

--- math/fastor/include/algebra/math/impl/fastor_vector.hpp
+++ math/fastor/include/algebra/math/impl/fastor_vector.hpp
@@ -11,8 +11,6 @@
 #include "algebra/math/common.hpp"
 #include "algebra/qualifiers.hpp"
 
-// System include(s).
-
 namespace algebra::fastor::math {
 
 /** Get a normalized version of the input vector
@@ -24,6 +22,36 @@ ALGEBRA_HOST inline Fastor::Tensor<scalar_t, N> normalize(
     const Fastor::Tensor<scalar_t, N> &v) {
 
   return Fastor::norm(v);
+}
+
+/** Dot product between two input vectors
+ *
+ *
+ * @param a the first input vector
+ * @param b the second input vector
+ *
+ * @return the scalar dot product value
+ **/
+template <typename scalar_t, auto N>
+ALGEBRA_HOST_DEVICE inline auto dot(
+    const Fastor::Tensor<scalar_t, N> &a,
+    const Fastor::Tensor<scalar_t, N> &b) {
+  return Fastor::inner(a, b);
+}
+
+/** Cross product between two input vectors
+ *
+ *
+ * @param a the first input vector
+ * @param b the second input vector
+ *
+ * @return a vector (expression) representing the cross product
+ **/
+template <typename scalar_t, auto N>
+ALGEBRA_HOST_DEVICE inline auto cross(
+    const Fastor::Tensor<scalar_t, N> &a,
+    const Fastor::Tensor<scalar_t, N> &b) {
+  return Fastor::cross(a, b);
 }
 
 }  // namespace algebra::fastor::math

cba0bce1222a9703c1d70183788f820a8c17ecf8
wermos <63574588+wermos@users.noreply.github.com>
2022-11-01 10:32:38 +0530

    Minor fixes.

--- math/fastor/include/algebra/math/impl/fastor_vector.hpp
+++ math/fastor/include/algebra/math/impl/fastor_vector.hpp
@@ -1,6 +1,6 @@
 /** Algebra plugins, part of the ACTS project
  *
- * (c) 2020-2022 CERN for the benefit of the ACTS project
+ * (c) 2022 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -21,11 +21,10 @@ template <typename scalar_t, auto N>
 ALGEBRA_HOST inline Fastor::Tensor<scalar_t, N> normalize(
     const Fastor::Tensor<scalar_t, N> &v) {
 
-  return Fastor::norm(v);
+  return Fastor::evaluate(Fastor::norm(v));
 }
 
 /** Dot product between two input vectors
- *
  *
  * @param a the first input vector
  * @param b the second input vector
@@ -33,25 +32,22 @@ ALGEBRA_HOST inline Fastor::Tensor<scalar_t, N> normalize(
  * @return the scalar dot product value
  **/
 template <typename scalar_t, auto N>
-ALGEBRA_HOST_DEVICE inline auto dot(
-    const Fastor::Tensor<scalar_t, N> &a,
-    const Fastor::Tensor<scalar_t, N> &b) {
-  return Fastor::inner(a, b);
+ALGEBRA_HOST_DEVICE inline auto dot(const Fastor::Tensor<scalar_t, N> &a,
+                                    const Fastor::Tensor<scalar_t, N> &b) {
+  return Fastor::evaluate(Fastor::inner(a, b));
 }
 
 /** Cross product between two input vectors
- *
  *
  * @param a the first input vector
  * @param b the second input vector
  *
  * @return a vector (expression) representing the cross product
  **/
-template <typename scalar_t, auto N>
-ALGEBRA_HOST_DEVICE inline auto cross(
-    const Fastor::Tensor<scalar_t, N> &a,
-    const Fastor::Tensor<scalar_t, N> &b) {
-  return Fastor::cross(a, b);
+template <typename scalar_t>
+ALGEBRA_HOST_DEVICE inline auto cross(const Fastor::Tensor<scalar_t, 3> &a,
+                                      const Fastor::Tensor<scalar_t, 3> &b) {
+  return Fastor::evaluate(Fastor::cross(a, b));
 }
 
 }  // namespace algebra::fastor::math

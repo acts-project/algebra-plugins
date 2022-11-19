0d6209f57230f406734f3e283abc79aee810530d
wermos <63574588+wermos@users.noreply.github.com>
2022-11-20 01:20:50 +0530

    Added definitions for dot and cross where one or more inputs are 3x1 matrices.

--- math/fastor/include/algebra/math/impl/fastor_vector.hpp
+++ math/fastor/include/algebra/math/impl/fastor_vector.hpp
@@ -34,9 +34,57 @@ ALGEBRA_HOST inline Fastor::Tensor<scalar_t, N> normalize(
  * @return the scalar dot product value
  **/
 template <typename scalar_t, auto N>
-ALGEBRA_HOST_DEVICE inline auto dot(const Fastor::Tensor<scalar_t, N> &a,
+ALGEBRA_HOST_DEVICE inline scalar_t dot(const Fastor::Tensor<scalar_t, N> &a,
                                     const Fastor::Tensor<scalar_t, N> &b) {
   return Fastor::inner(a, b);
+}
+
+/** Dot product between Tensor<scalar_t, N> and Tensor<scalar_t, N, 1>
+ *
+ * @param a the first input vector
+ * @param b the second input Tensor<scalar_t, N, 1>
+ *
+ * @return the scalar dot product value
+ **/
+template <typename scalar_t, auto N>
+ALGEBRA_HOST inline scalar_t dot(
+    const Fastor::Tensor<scalar_t, N> &a,
+    const Fastor::Tensor<scalar_t, N, 1> &b) {
+
+  // We need to specify the type of the Tensor slice because Fastor by default
+  // is lazy, so it returns an intermediate type which does not play well with
+  // the Fastor::inner function.
+  return Fastor::inner(a, Fastor::Tensor<scalar_t, N>(b(Fastor::fseq<0, N>(), 0)));
+}
+
+/** Dot product between Tensor<scalar_t, N> and Tensor<scalar_t, N, 1>
+ *
+ * @param a the second input Tensor<scalar_t, N, 1>
+ * @param b the first input vector
+ *
+ * @return the scalar dot product value
+ **/
+template <typename scalar_t, auto N>
+ALGEBRA_HOST inline scalar_t dot(
+    const Fastor::Tensor<scalar_t, N, 1> &a,
+    const Fastor::Tensor<scalar_t, N> &b) {
+
+  return Fastor::inner(Fastor::Tensor<scalar_t, N>(a(Fastor::fseq<0, N>(), 0)), b);
+}
+
+/** Dot product between two Tensor<scalar_t, 3, 1>
+ *
+ * @param a the second input Tensor<scalar_t, 3, 1>
+ * @param b the first input Tensor<scalar_t, 3, 1>
+ *
+ * @return the scalar dot product value
+ **/
+template <typename scalar_t, auto N>
+ALGEBRA_HOST inline scalar_t dot(
+    const Fastor::Tensor<scalar_t, N, 1> &a,
+    const Fastor::Tensor<scalar_t, N, 1> &b) {
+
+  return Fastor::inner(Fastor::Tensor<scalar_t, N>(a(Fastor::fseq<0, 3>(), 0)), Fastor::Tensor<scalar_t, N>(b(Fastor::fseq<0, 3>(), 0)));
 }
 
 /** Cross product between two input vectors
@@ -47,9 +95,57 @@ ALGEBRA_HOST_DEVICE inline auto dot(const Fastor::Tensor<scalar_t, N> &a,
  * @return a vector (expression) representing the cross product
  **/
 template <typename scalar_t>
-ALGEBRA_HOST_DEVICE inline auto cross(const Fastor::Tensor<scalar_t, 3> &a,
+ALGEBRA_HOST_DEVICE inline Fastor::Tensor<scalar_t, 3> cross(const Fastor::Tensor<scalar_t, 3> &a,
                                       const Fastor::Tensor<scalar_t, 3> &b) {
   return Fastor::cross(a, b);
+}
+
+/** Cross product between Tensor<scalar_t, 3> and Tensor<scalar_t, 3, 1>
+ *
+ * @param a the first input vector
+ * @param b the second input Tensor<scalar_t, 3, 1>
+ *
+ * @return a vector representing the cross product
+ **/
+template <typename scalar_t>
+ALGEBRA_HOST inline Fastor::Tensor<scalar_t, 3> cross(
+    const Fastor::Tensor<scalar_t, 3> &a,
+    const Fastor::Tensor<scalar_t, 3, 1> &b) {
+
+  // We need to specify the type of the Tensor slice because Fastor by default
+  // is lazy, so it returns an intermediate type which does not play well with
+  // the Fastor::cross function.
+  return Fastor::cross(a, Fastor::Tensor<scalar_t, 3>(b(Fastor::fseq<0, 3>(), 0)));
+}
+
+/** Cross product between Tensor<scalar_t, 3> and Tensor<scalar_t, 3, 1>
+ *
+ * @param a the second input Tensor<scalar_t, 3, 1>
+ * @param b the first input vector
+ *
+ * @return a vector representing the cross product
+ **/
+template <typename scalar_t>
+ALGEBRA_HOST inline Fastor::Tensor<scalar_t, 3> cross(
+    const Fastor::Tensor<scalar_t, 3, 1> &a,
+    const Fastor::Tensor<scalar_t, 3> &b) {
+
+  return Fastor::cross(Fastor::Tensor<scalar_t, 3>(a(Fastor::fseq<0, 3>(), 0)), b);
+}
+
+/** Cross product between two Tensor<scalar_t, 3, 1>
+ *
+ * @param a the second input Tensor<scalar_t, 3, 1>
+ * @param b the first input Tensor<scalar_t, 3, 1>
+ *
+ * @return a vector representing the cross product
+ **/
+template <typename scalar_t>
+ALGEBRA_HOST inline Fastor::Tensor<scalar_t, 3> cross(
+    const Fastor::Tensor<scalar_t, 3, 1> &a,
+    const Fastor::Tensor<scalar_t, 3, 1> &b) {
+
+  return Fastor::cross(Fastor::Tensor<scalar_t, 3>(a(Fastor::fseq<0, 3>(), 0)), Fastor::Tensor<scalar_t, 3>(b(Fastor::fseq<0, 3>(), 0)));
 }
 
 }  // namespace algebra::fastor::math

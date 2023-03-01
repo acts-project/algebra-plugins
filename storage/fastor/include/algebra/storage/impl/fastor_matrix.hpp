fd2b21b2fd3a805c85f6bec9dca4be579eeff549
wermos <63574588+wermos@users.noreply.github.com>
2023-03-01 08:12:08 +0530

    Formatted the files and removed some duplication.

--- storage/fastor/include/algebra/storage/impl/fastor_matrix.hpp
+++ storage/fastor/include/algebra/storage/impl/fastor_matrix.hpp
@@ -24,33 +24,24 @@ class Matrix : public Fastor::Tensor<T, M1, N> {
   using Fastor::Tensor<T, M1, N>::Tensor;
 
   template <typename U, std::size_t M2,
-  			  std::enable_if_t<std::is_convertible_v<U, T>, bool> = true
-    >
+            std::enable_if_t<std::is_convertible_v<U, T>, bool> = true>
   inline Matrix<T, M1, M2> operator*(const Matrix<U, N, M2>& other) {
-  	return Fastor::matmul(static_cast<Fastor::Tensor<T, M1, N>>(*this), static_cast<Fastor::Tensor<T, N, M2>>(other));
-  }
-
-  template <typename U, std::size_t M2,
-              std::enable_if_t<std::is_convertible_v<U, T>, bool> = true
-    >
-  inline Matrix<T, M1, M2> operator*(const Fastor::Tensor<U, N, M2>& other) {
-  	return Fastor::matmul(static_cast<Fastor::Tensor<T, M1, N>>(*this), static_cast<Fastor::Tensor<T, N, M2>>(other));
+    return Fastor::matmul(static_cast<Fastor::Tensor<T, M1, N>>(*this),
+                          static_cast<Fastor::Tensor<T, N, M2>>(other));
   }
 
   template <typename U,
-              std::enable_if_t<std::is_convertible_v<U, T>, bool> = true
-    >
+            std::enable_if_t<std::is_convertible_v<U, T>, bool> = true>
   inline auto operator*(const Fastor::Tensor<U, N>& other) {
-  	return Fastor::matmul(*this, other);
+    return Fastor::matmul(*this, other);
   }
 
   template <typename U,
-              std::enable_if_t<std::is_convertible_v<U, T>, bool> = true
-    >
+            std::enable_if_t<std::is_convertible_v<U, T>, bool> = true>
   inline Fastor::Tensor<T, M1, N> operator*(const U scalar) {
-  	return static_cast<T>(scalar) * (*this);
+    return static_cast<T>(scalar) * (*this);
   }
 
-};  // class Matrix 
+};  // class Matrix
 
 }  // namespace algebra::fastor

6e94c727ae78e6c1b93893e7818e3ebd89c2af17
wermos <63574588+wermos@users.noreply.github.com>
2023-03-09 20:42:41 +0530

    Removed some unnecessary operations.

--- math/fastor/include/algebra/math/impl/fastor_transform3.hpp
+++ math/fastor/include/algebra/math/impl/fastor_transform3.hpp
@@ -85,7 +85,6 @@ struct transform3 {
 
     // The matrices need to be initialized to the identity matrix first.
     _data.eye2();
-    _data_inv.eye2();
 
     _data(Fastor::fseq<0, 3>(), 0) = x;
     _data(Fastor::fseq<0, 3>(), 1) = y;
@@ -116,9 +115,8 @@ struct transform3 {
   ALGEBRA_HOST
   transform3(const vector3 &t) {
 
-    // The matrices need to be initialized to the identity matrix first.
+    // The matrix needs to be initialized to the identity matrix first.
     _data.eye2();
-    _data_inv.eye2();
 
     _data(Fastor::fseq<0, 3>(), 3) = t;
 

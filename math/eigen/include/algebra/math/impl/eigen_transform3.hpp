9bdbdc8834aa59940cb4dada9742fefe16e8216f
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2023-10-09 13:14:11 +0200

    Turned off specific CUDA warnings for the Eigen headers.
    This is only needed with MSVC, since "system include directories"
    as a concept doesn't quite work with that compiler. So we need to
    explicitly turn off the checking for some warnings in the code
    like this.

--- math/eigen/include/algebra/math/impl/eigen_transform3.hpp
+++ math/eigen/include/algebra/math/impl/eigen_transform3.hpp
@@ -1,6 +1,6 @@
 /** Algebra plugins library, part of the ACTS project
  *
- * (c) 2020-2022 CERN for the benefit of the ACTS project
+ * (c) 2020-2023 CERN for the benefit of the ACTS project
  *
  * Mozilla Public License Version 2.0
  */
@@ -14,11 +14,20 @@
 #ifdef _MSC_VER
 #pragma warning(push, 0)
 #endif  // MSVC
+#ifdef __NVCC_DIAG_PRAGMA_SUPPORT__
+#pragma nv_diagnostic push
+#pragma nv_diag_suppress 20011
+#pragma nv_diag_suppress 20012
+#pragma nv_diag_suppress 20014
+#endif  // __NVCC_DIAG_PRAGMA_SUPPORT__
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif  // MSVC
+#ifdef __NVCC_DIAG_PRAGMA_SUPPORT__
+#pragma nv_diagnostic pop
+#endif  // __NVCC_DIAG_PRAGMA_SUPPORT__
 
 // System include(s).
 #include <type_traits>

8724748028287c393c0fb5daea0e31584c0acea0
wermos <63574588+wermos@users.noreply.github.com>
2025-03-14 02:24:49 +0530

    Added the transform3 benchmarks for Fastor.

---
+++ benchmarks/fastor/fastor_transform3.cpp
@@ -0,0 +1,45 @@
+/** Algebra plugins library, part of the ACTS project
+ *
+ * (c) 2025 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Project include(s)
+#include "algebra/fastor_fastor.hpp"
+#include "benchmark/common/benchmark_transform3.hpp"
+#include "benchmark/common/register_benchmark.hpp"
+#include "benchmark/fastor/data_generator.hpp"
+
+// Benchmark include
+#include <benchmark/benchmark.h>
+
+using namespace algebra;
+
+/// Run vector benchmarks
+int main(int argc, char** argv) {
+
+  //
+  // Prepare benchmarks
+  //
+  algebra::benchmark_base::configuration cfg{};
+  cfg.n_samples(100000);
+
+  using trf_f_t = transform3_bm<fastor::transform3<float>>;
+  using trf_d_t = transform3_bm<fastor::transform3<double>>;
+
+  std::cout << "-----------------------------------------------\n"
+            << "Algebra-Plugins 'transform3' benchmark (Fastor)\n"
+            << "-----------------------------------------------\n\n"
+            << cfg;
+
+  //
+  // Register all benchmarks
+  //
+  algebra::register_benchmark<trf_f_t>(cfg, "_single");
+  algebra::register_benchmark<trf_d_t>(cfg, "_double");
+
+  ::benchmark::Initialize(&argc, argv);
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+}

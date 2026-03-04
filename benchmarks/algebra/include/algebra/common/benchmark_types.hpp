5f37292b21217609c3cc1e7ea1fbd7c900554168
Joana Niermann <53186085+niermann999@users.noreply.github.com>
2026-03-04 13:43:25 +0100

    ref: refactor benchmarks (#172)
    Deduplicate the benchmarks similar to the unit tests and restructure the benchmark lib closer to what is used in detray

---
+++ benchmarks/algebra/include/algebra/common/benchmark_types.hpp
@@ -0,0 +1,62 @@
+/** Algebra plugins library, part of the ACTS project
+ *
+ * (c) 2026 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+#pragma once
+
+// Algebra plugin selection
+#if ALGEBRA_BENCHMARK_ARRAY
+#include "algebra/array.hpp"
+#include "algebra/array/data_generator.hpp"
+#endif
+#if ALGEBRA_BENCHMARK_EIGEN
+#include "algebra/eigen.hpp"
+#include "algebra/eigen/data_generator.hpp"
+#endif
+#if ALGEBRA_BENCHMARK_FASTOR
+#include "algebra/fastor.hpp"
+#include "algebra/fastor/data_generator.hpp"
+#endif
+#if ALGEBRA_BENCHMARK_SMATRIX
+#include "algebra/smatrix.hpp"
+#include "algebra/smatrix/data_generator.hpp"
+#endif
+#if ALGEBRA_BENCHMARK_VC_AOS
+#include "algebra/vc_aos.hpp"
+#include "algebra/vc_aos/data_generator.hpp"
+#endif
+
+// Project include(s).
+#include "algebra/common/benchmark_vector.hpp"
+
+// Benchmark include
+#include <benchmark/benchmark.h>
+
+// System include(s)
+#include <iostream>
+#include <string>
+
+namespace algebra::benchmark {
+
+#define ALGEBRA_DEFINE_BENCHMARK_NAME(ALGEBRA) \
+  static std::string plugin_name(#ALGEBRA);
+
+// Select algebra-plugin to compile the test with
+#if ALGEBRA_BENCHMARK_ARRAY
+ALGEBRA_DEFINE_BENCHMARK_NAME(array)
+#elif ALGEBRA_BENCHMARK_EIGEN
+ALGEBRA_DEFINE_BENCHMARK_NAME(eigen)
+#elif ALGEBRA_BENCHMARK_FASTOR
+ALGEBRA_DEFINE_BENCHMARK_NAME(fastor)
+#elif ALGEBRA_BENCHMARK_SMATRIX
+ALGEBRA_DEFINE_BENCHMARK_NAME(smatrix)
+#elif ALGEBRA_BENCHMARK_VC_AOS
+ALGEBRA_DEFINE_BENCHMARK_NAME(vc_aos)
+#elif ALGEBRA_BENCHMARK_VC_SOA
+ALGEBRA_DEFINE_BENCHMARK_NAME(vc_soa)
+#endif
+
+}  // namespace algebra::benchmark
---
+++ benchmarks/algebra/src/benchmark_getter.cpp
@@ -0,0 +1,44 @@
+/** Algebra plugins library, part of the ACTS project
+ *
+ * (c) 2023-2026 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+// Project include(s)
+// clang-format off
+#include "algebra/common/benchmark_types.hpp"
+#include "algebra/common/benchmark_getter.hpp"
+// clang-format on
+
+// Benchmark include
+#include <benchmark/benchmark.h>
+
+// System include(s)
+#include <iostream>
+
+using namespace algebra;
+
+/// Run vector benchmarks
+int main(int argc, char** argv) {
+
+  //
+  // Prepare benchmarks
+  //
+  algebra::benchmark_base::configuration cfg{};
+  cfg.n_samples(100000);
+
+  std::cout << "-----------------------------------------------\n"
+            << "Algebra-Plugins 'getter' benchmark ("
+            << algebra::benchmark::plugin_name << ")\n"
+            << "-----------------------------------------------\n\n"
+            << cfg;
+
+  //
+  // Register all benchmarks
+  //
+
+  ::benchmark::Initialize(&argc, argv);
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+}
